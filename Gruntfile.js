6e3ac187d9b1e866abc4383cda8467e44e8f7f19
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-09-21 18:24:02 -0700

    fdsa

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,7 +134,7 @@ module.exports = function(grunt) {
 					browsers: [].concat(
 						safari_browsers,
 						chrome_browsers,
-						// firefox_browsers,
+						firefox_browsers,
 						android_browsers,
 						ios_browsers,
 						ie_browsers)
